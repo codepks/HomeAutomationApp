41c54c1c9ac6f3d00a1e31b443bef15e6ec66852
prashant kumar sharma <faith.prashant@gmail.com>
2019-03-23 04:02:10 +0530

    Beautified Background and Texts

---
+++ appldoreV1/www5 - AllCompleteBase/js/slider.js
@@ -0,0 +1,10 @@
+//=================================CORE FOR SLIDER====================================
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
